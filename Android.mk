34afbc253539067c68f33acbd9be8df7da1c4ecd
Bruno Martins <bgcngm@gmail.com>
2018-08-31 00:47:58 +0100

    WeatherProvider: Build with AAPT2
    Change-Id: I909164dd2b40bec2c775b30d118f97564c8a2a08

--- Android.mk
+++ Android.mk
@@ -3,12 +3,17 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_USE_AAPT2 := true
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v4 \
     android-support-v7-appcompat \
     android-support-v7-preference \
     android-support-v7-recyclerview \
-    android-support-v14-preference \
+    android-support-v14-preference
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
     org.lineageos.platform.internal
 
 LOCAL_MODULE_TAGS := optional
